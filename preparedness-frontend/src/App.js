7302f3d2fc9c45a54f1acaa4a24204ba82b198b2
Deanna "Will" Williams <deannawilliams@Deannas-MacBook-Pro.local>
2023-05-11 16:27:28 -0700

    completed

--- preparedness-frontend/src/App.js
+++ preparedness-frontend/src/App.js
@@ -1,20 +1,39 @@
-import React from "react"
+import React, { useState } from "react"
 import { Button, Input, Label } from "reactstrap"
 import ModalComponent from "./components/ModalComponent"
 import "./App.css"
 
 const App = () => {
+
+  const [userName, setUserName] = useState(" ")
+
+  const handleChange = (e) => {
+    setUserName(e.target.value)
+  }
+
+  const [showModal, setShowModal] = useState(false)
+
+  const handleToggle = () => {
+    setShowModal(!showModal)
+  }
+  
+  const handleReset = () => {
+    setUserName(" ")
+  }
+
   return (
     <div className="entire-content">
       <h1>Preparedness Assessment</h1>
       <div className="form">
         <div className="input">
-          <Label for="name">Enter your name</Label>
-          <Input />
+          <Label className="label" for="name">Enter your name</Label>
+          <Input className="textfield" value={userName} onChange={handleChange}/>
         </div>
-        <Button>Click Me</Button>
-        <Button>Reset</Button>
-        <ModalComponent />
+        <div className="button">
+          <Button className="clickMe" onClick={handleToggle}>Click Me</Button>
+          <Button className="clickMe" onClick={handleReset}>Reset</Button>
+        </div>
+        <ModalComponent userName={userName} handleToggle={handleToggle} showModal={showModal}/>
       </div>
     </div>
   )
--- preparedness-frontend/src/components/ModalComponent.js
+++ preparedness-frontend/src/components/ModalComponent.js
@@ -1,7 +1,25 @@
 import React from "react"
+import { Modal, ModalHeader, ModalBody} from 'reactstrap'
 
-const ModalComponent = () => {
-  return <></>
+const ModalComponent = ({userName , showModal, handleToggle}) => {
+  // console.log({userName})
+  return (
+  <div>
+      <Modal isOpen={showModal}>
+        <ModalHeader toggle={handleToggle} className="modalHeader"> Hello there {userName} !</ModalHeader>
+        
+        <ModalBody>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </ModalBody>
+      </Modal>
+    </div>
+  )
 }
 
 export default ModalComponent
